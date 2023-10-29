c64ae7c0637505ad3ae9df766828aaecaf3b33f2
Rakib Hossen <rakibhossen1660@gmail.com>
2023-10-29 13:10:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sidebar from './component/sidebar/Sidebar'
+import MainDash from './component/MainDash/MainDash'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar/>
+        <MainDash/>
+        {/* <RightSide/> */}
+      </div>
+    </div>
+  )
+}
+
+export default App
