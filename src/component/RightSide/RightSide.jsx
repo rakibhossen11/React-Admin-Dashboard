f91ad7b9abdb8624f40f3637ed921b8bfbd982e5
Rakib Hossen <rakibhossen1660@gmail.com>
2023-10-29 14:34:56 +0600

    create dash complete

--- src/component/RightSide/RightSide.jsx
+++ src/component/RightSide/RightSide.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-// import CustomerReview from "../CustomerReview/CustomerReview";
-// import Updates from "../Updates/Updates";
+import Update from "../updates/Update";
+import CustomerReview from "../customerReview/CustomerReview";
+
 import "./RightSide.css";
 
 const RightSide = () => {
@@ -8,11 +9,11 @@ const RightSide = () => {
     <div className="RightSide">
       <div>
         <h3>Updates</h3>
-        {/* <Updates /> */}
+        <Update />
       </div>
       <div>
         <h3>Customer Review</h3>
-        {/* <CustomerReview /> */}
+        <CustomerReview />
       </div>
     </div>
   );
